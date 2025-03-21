23c7200f107b150efe64d32bbe22ce8f1fe05b86
Benjamin Werner <wernerbenjamin42@gmail.com>
2025-03-21 16:33:08 +0100

    add cg-bin/cgi_info.sh script that prints out arguments and env

--- cgi-bin/hello.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-asdfasdfsf
-echo "Content-Type: text/html"
-echo ""
-echo "Hello world!"
-exit 1
--- cgi-bin/upload.cgi
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# Generate response
-echo "Content-type: text/html"
-echo ""
-echo "$(env)"
